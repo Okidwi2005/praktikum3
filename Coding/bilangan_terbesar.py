e43ee498a74a4b685143bfcaa0293099ed47d599
Okidwi2005 <okisaputro17@gmail.com>
2024-10-26 13:11:34 +0700

    Create bilangan_terbesar.py

---
+++ Coding/bilangan_terbesar.py
@@ -0,0 +1,10 @@
+max = 0
+
+while True:
+    bilangan = int(input("Masukan bilangan(0 untuk berhenti): "))
+    if bilangan == 0:
+        break
+    if bilangan > max:
+        max = bilangan
+
+print(f"Bilangan terbesar: {max}")
